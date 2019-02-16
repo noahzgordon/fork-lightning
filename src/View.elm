79284083f06c15359030b1cce4ab97baa7f8fb50
Noah Gordon <noahzacharygordon@gmail.com>
2019-02-16 14:07:16 -0500

    black canvas

--- src/View.elm
+++ src/View.elm
@@ -1,10 +1,27 @@
 module View exposing (draw)
 
+import Color exposing (rgb)
 import Html exposing (Html)
 import Messages exposing (Message)
 import Model exposing (..)
+import TypedSvg exposing (..)
+import TypedSvg.Attributes as Attributes exposing (..)
+import TypedSvg.Core exposing (..)
+import TypedSvg.Types exposing (..)
 
 
 draw : Model -> List (Html Message)
 draw model =
-    [ Html.text "hello world" ]
+    [ svg
+        [ width (model.window.width |> px)
+        , height (model.window.height |> px)
+        , Attributes.style "position: absolute; top: 0; left: 0;"
+        ]
+        [ rect
+            [ width (100 |> percent)
+            , height (100 |> percent)
+            , fill (rgb 0 0 0 |> Fill)
+            ]
+            []
+        ]
+    ]
